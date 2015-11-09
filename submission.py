8608157d27e738c5adccfb7003b2ba6010b30dbd
tvme <ersan@tvme.ru>
2015-11-09 23:41:54 +0300

    prepare file submissin from DataFrame, more faster then old vertion

--- submission.py
+++ submission.py
@@ -4,35 +4,33 @@ __author__ = 'Ersan'
 import os
 import pandas as pd
 
+id_stock = 1
 
-def data_to_submission(row):
+def id_submission(row):
     """
     read row by row from DataFame
     concat columns to data_for_submission (in start empty DataFrame)
-    :return:
+    :return: text columns Id,Predicted
     """
-    global id_stock, data_for_submission
-    id_name = []
+    global id_stock
+    test_out = ''
     for ndx in range(len(row)):
-        name = str(id_stock) + '_' + str(ndx + 1)
-        id_name.append(name)
-    id_series = pd.DataFrame(row.values, index=id_name, columns=['Predicted'])
+        id_name = str(id_stock) + '_' + str(ndx + 1)
+        test_out += id_name + ',' + str(row.values[ndx]) + '\n'
     id_stock += 1
-    data_for_submission = pd.concat([data_for_submission, id_series])
-    return
+    return test_out
+
 
 if __name__ == '__main__':
     dir_path = os.getcwdu() + '\data\\'
+    f_submission = 'test_submission.csv'
     f_train_in = 'train.csv'
-    # f_test_in = 'test.csv'
-    f_submission = 'sumission.csv'
     f_train_1000 = 'train_1000.csv'
     f_data_test = 'dummy_data.csv'
-    id_stock = 1 # global index
 
-    df = pd.read_csv(dir_path + f_data_test, sep=None, engine='python') # read data for submission
-    data_for_submission = pd.DataFrame(columns=['Predicted']) # empty DataFrame for submission
-    df = df.loc[:, 'Ret_121':'Ret_PlusTwo'].apply(data_to_submission, axis=1).copy
-    data_for_submission.index.name = 'Id'
-    print data_for_submission
-    data_for_submission.to_csv(dir_path+f_submission)
+    df = pd.read_csv(dir_path + f_train_in, sep=None, engine='python') # read data for submission
+
+    with open(dir_path+f_submission, 'w') as f_target:
+        line_zero = 'Id,Predicted\n'
+        f_target.write(line_zero)
+        df.loc[:, 'Ret_121':'Ret_PlusTwo'].apply(lambda row: f_target.write(id_submission(row)), axis=1)
